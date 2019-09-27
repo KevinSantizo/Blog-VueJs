f72c416b0ef7fcb11b89c8ee32150461ed916421
KevinSantizo <kevinsantizo660@gmail.com>
2019-09-27 05:21:08 -0600

    Blog Vue.js tutorial 35

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <show-blogs> </show-blogs>
+    </div>
+</template>
+
+<script>
+// Imports
+import addBlog from './components/addBlog.vue'
+import showBlogs from './components/showBlogs.vue'
+
+export default {
+    components: {
+        'add-blog': addBlog,
+        'show-blogs': showBlogs
+    },
+    data () {
+        return {
+        }
+    },
+    methods: {
+     
+    }
+}
+</script>
+
+<style>
+body{
+    margin: 0;
+    font-family: 'Nunito SemiBold';
+}
+</style>
